bd1edaaa092668851cde1a99ccf1a600ac99fc7b
David Terry <me@xwvvvvwx.com>
2020-04-29 10:13:09 +0200

    add shell.nix, fix text with gnused

---
+++ shell.nix
@@ -0,0 +1,24 @@
+let
+  pkgs = import <nixpkgs> {};
+
+  dappSrc = builtins.fetchGit rec {
+    name = "dapptools-${rev}";
+    url = https://github.com/dapphub/dapptools;
+    # provides solc 0.5.16
+    rev = "96c4bfd228cf6e116c2d93f3e89d795bcd8b5511";
+    ref = "master";
+  };
+  dapptools = import dappSrc {};
+
+in
+  pkgs.mkShell {
+    name = "k-uniswap";
+    buildInputs = with pkgs; [
+      gnused
+      dapptools.dapp
+    ];
+    shellHook = ''
+      export NIX_PATH="nixpkgs=${pkgs.path}"
+      export DAPPTOOLS=${dappSrc}
+    '';
+  }
